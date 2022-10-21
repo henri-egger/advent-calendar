a66c3f527b9d500d2c96e83bf098066c3212bacb
henri <henri@moosbauer.com>
2022-10-21 11:24:35 +0200

    add example modal

--- src/App.js
+++ src/App.js
@@ -5,12 +5,55 @@ import Foreground from "./Foreground";
 
 const App = () => {
     return (
-        <Background>
-            <img className="col-6 col-md-4 col-lg-3 py-4" src={logo} alt="logo"></img>
-            <div className="col">
-                <Foreground />
+        <>
+            <Background>
+                <img
+                    className="col-6 col-md-4 col-lg-3 py-4"
+                    src={logo}
+                    alt="logo"
+                ></img>
+                <div className="col">
+                    <Foreground />
+                </div>
+            </Background>
+
+            <div
+                class="modal fade"
+                id="exampleModal"
+                tabindex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">
+                                Modal title
+                            </h1>
+                            <button
+                                type="button"
+                                class="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            ></button>
+                        </div>
+                        <div class="modal-body">...</div>
+                        <div class="modal-footer">
+                            <button
+                                type="button"
+                                class="btn btn-secondary"
+                                data-bs-dismiss="modal"
+                            >
+                                Close
+                            </button>
+                            <button type="button" class="btn btn-primary">
+                                Save changes
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </Background>
+        </>
     );
 };
 
--- src/Window.js
+++ src/Window.js
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Window = (props) => {
     return (
-        <div className='col'>
-            <div className='window p-4'>
+        <div className="col">
+            <div
+                className="window p-4"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+            >
                 <h3>{props.day}</h3>
             </div>
         </div>
     );
-}
- 
+};
+
 export default Window;
